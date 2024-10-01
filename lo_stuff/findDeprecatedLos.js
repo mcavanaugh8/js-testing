01b176c29692c101af7314acd4c56588953a7d1a
mcavanaugh8 <milecav8@gmail.com>
2024-10-01 12:41:25 -0400

    changes

--- lo_stuff/findDeprecatedLos.js
+++ lo_stuff/findDeprecatedLos.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const AdmZip = require('adm-zip');
+var zipper = require('zip-local');
 const { DOMParser } = require('xmldom');
 const convert = require('xml-js');
 const xpath = require('xpath');
@@ -13,17 +13,37 @@ const reportFile = path.join(__dirname, 'report.tsv');
 
 /** unzip all docx files */
 const filesFolder = fs.readdirSync(path.join(__dirname, 'files'));
+const filesFolderPath = path.join(__dirname, 'files');
 const outputFolder = path.join(__dirname, 'xml');
 
 // Use an async function to handle the asynchronous nature of mammoth
 (async () => {
+
+    for (const file of filesFolder) {
+        if (path.extname(file) === '.zip' && file !== '.DS_Store') {
+            console.log(`${filesFolderPath}/${file}`);
+            try {
+                var unzippedfs = zipper.sync.unzip(`${filesFolderPath}/${file}`).memory();
+                console.log(unzippedfs.contents());
+                
+                unzippedfs.contents().forEach(unzippedFile => {
+                    var buff = unzippedfs.read(unzippedFile, "buffer");
+                    fs.writeFileSync(`${filesFolderPath}/${unzippedFile}`, buff)                    
+                })
+            } catch (err) {
+                console.error(`Error unzipping file ${file}:`, err);
+            }
+
+        }
+    }
+
     for (const file of filesFolder) {
         if (path.extname(file) === '.docx') {
             try {
                 let content = await extractTextFromDocx(path.join(__dirname, 'files', file));
-    
+
                 let matches = content.match(/(RL|RI|L|W|SL|RF)(\.)([A-Z]{1,4})(\.)(\d\d*)(\.)([a-z])*(\.)*(\d|K)*(\.)*([a-z]{1,4})*[^\s]+/g);
-    
+
                 if (matches) {
                     matches.forEach(match => {
                         if (salt_los.filter(lo => lo.humanCodingScheme === match).length > 0) {
